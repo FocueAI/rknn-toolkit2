ee6071899189295a578c679b2f477b2a305fbf90
FocueAI <17051325951@163.com>
2024-11-11 16:32:49 +0800

    该版本已经可以很粗糙的定位出书脊的位置了，但是box 的scale还需进一步调整

--- rknn-toolkit2/examples/onnx/Db-mul/test.py
+++ rknn-toolkit2/examples/onnx/Db-mul/test.py
@@ -6,14 +6,17 @@ import sys
 import numpy as np
 import cv2, math
 from rknn.api import RKNN
+from shapely.geometry import Point, LineString, Polygon
+from shapely.geometry import Polygon
+import pyclipper
 
 # Model from https://github.com/airockchip/rknn_model_zoo
-ONNX_MODEL = 'final_2024_05_05_17_02.onnx'
-RKNN_MODEL = 'final_2024_05_05_17_02.rknn'
-IMG_PATH = './bus.jpg'
-DATASET = './dataset.txt'
+ONNX_MODEL = '/usr/projects/explore/rknn-toolkit2/rknn-toolkit2/examples/onnx/Db-mul/final_2024_05_05_17_02.onnx'
+RKNN_MODEL = '/usr/projects/explore/rknn-toolkit2/rknn-toolkit2/examples/onnx/Db-mul/final_2024_05_05_17_02.rknn'
+IMG_PATH = '/usr/projects/explore/rknn-toolkit2/rknn-toolkit2/examples/onnx/Db-mul/IMG_20191014_161409.jpg'
+DATASET = '/usr/projects/explore/rknn-toolkit2/rknn-toolkit2/examples/onnx/Db-mul/dataset.txt'
 
-QUANTIZE_ON =  True
+QUANTIZE_ON =  False
 
 def letterbox(im, new_shape=(640, 640), color=(0, 0, 0)):
     # Resize and pad image while meeting stride-multiple constraints
@@ -22,7 +25,7 @@ def letterbox(im, new_shape=(640, 640), color=(0, 0, 0)):
         new_shape = (new_shape, new_shape)
 
     # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1]) # 看看谁变化最小
 
     # Compute padding
     ratio = r, r  # width, height ratios
@@ -39,8 +42,131 @@ def letterbox(im, new_shape=(640, 640), color=(0, 0, 0)):
     im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
     return im, ratio, (dw, dh)
 
-if __name__ == '__main__':
 
+class SegDetector:
+    def __init__(self, **kargs):
+        self.thresh = kargs["thresh"]
+        self.max_candidates = kargs["max_candidates"]
+        self.min_size = kargs["min_size"]
+        self.box_thresh = kargs["box_thresh"]
+        
+        
+    def binarize(self, pred):
+        return pred > self.thresh
+    
+    def get_mini_boxes(self, contour):
+        bounding_box = cv2.minAreaRect(contour)
+        points = sorted(list(cv2.boxPoints(bounding_box)), key=lambda x: x[0])
+
+        index_1, index_2, index_3, index_4 = 0, 1, 2, 3
+        if points[1][1] > points[0][1]:
+            index_1 = 0
+            index_4 = 1
+        else:
+            index_1 = 1
+            index_4 = 0
+        if points[3][1] > points[2][1]:
+            index_2 = 2
+            index_3 = 3
+        else:
+            index_2 = 3
+            index_3 = 2
+
+        box = [points[index_1], points[index_2],
+               points[index_3], points[index_4]]
+        return box, min(bounding_box[1])
+    
+    def unclip(self, box, unclip_ratio=1.5):
+        poly = Polygon(box)
+        distance = poly.area * unclip_ratio / poly.length
+        offset = pyclipper.PyclipperOffset()
+        offset.AddPath(box, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
+        expanded = np.array(offset.Execute(distance))
+        return expanded
+    
+    def box_from_bitmap(self, pred, origin_im_size):
+        bitmap = self.binarize(pred)[0][0]
+        pred = pred[0]
+        height, width = pred.shape[1:]
+        contours, _ = cv2.findContours(
+            (bitmap * 255).astype(np.uint8),
+            cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        output = np.copy(bitmap)*255.0
+        output = np.ones_like(output)[:,:,None] * 255
+        output =output.repeat(3, axis=-1)
+        output = output.astype(np.uint8)
+        cv2.drawContours(output, contours, -1, (255, 255, 0), 2)
+        cv2.imwrite('contours.jpg', output)
+        
+        num_contours = min(len(contours), self.max_candidates)
+        boxes = np.zeros((num_contours, 4, 2), dtype=np.int16)
+        scores = np.zeros((num_contours,), dtype=np.float32)
+        
+        for index in range(num_contours):
+            contour = contours[index]
+            points, sside = self.get_mini_boxes(contour)
+            if sside < self.min_size:
+                continue
+            points = np.array(points)
+            score = self.box_score_fast(pred[0], points.reshape(-1, 2))     
+            if score < self.box_thresh:
+                continue
+            box = self.unclip(points, unclip_ratio=0.8).reshape(-1, 1, 2)
+            ############ hou 2024-4-22 #############
+            box_ = np.squeeze(box)
+            if Polygon(box_).area < 12000:
+                   continue
+            box, sside = self.get_mini_boxes(box)
+            box = np.array(box)
+            if sside < self.min_size + 2:
+                continue
+            #########################################
+            box[:, 0] = np.clip(
+                np.round(box[:, 0] / width * origin_im_size[1]), 0, origin_im_size[1])
+            box[:, 1] = np.clip(
+                np.round(box[:, 1] / height * origin_im_size[0]), 0, origin_im_size[0])
+            ####################################
+            box, sside = self.get_mini_boxes(box)
+            box = np.array(box)
+            if sside < self.min_size + 2:
+                continue
+            ####################################
+            boxes[index, :, :] = box.astype(np.int16)
+            scores[index] = score     
+        return boxes, scores          
+            
+    def box_score_fast(self, bitmap, _box):
+        h, w = bitmap.shape[:2]
+        box = _box.copy()
+        xmin = np.clip(np.floor(box[:, 0].min()).astype(np.int16), 0, w - 1) # np.int ---> np.int16 hou
+        xmax = np.clip(np.ceil(box[:, 0].max()).astype(np.int16), 0, w - 1)
+        ymin = np.clip(np.floor(box[:, 1].min()).astype(np.int16), 0, h - 1)
+        ymax = np.clip(np.ceil(box[:, 1].max()).astype(np.int16), 0, h - 1)
+
+        mask = np.zeros((ymax - ymin + 1, xmax - xmin + 1), dtype=np.uint8)
+        box[:, 0] = box[:, 0] - xmin
+        box[:, 1] = box[:, 1] - ymin
+        cv2.fillPoly(mask, box.reshape(1, -1, 2).astype(np.int32), 1)
+
+        return cv2.mean(bitmap[ymin:ymax + 1, xmin:xmax + 1], mask)[0]
+    
+    def represent(self, pred, origin_im_size):
+       boxes, scores = self.box_from_bitmap(pred, origin_im_size)
+       return boxes, scores
+
+
+
+
+
+if __name__ == '__main__':
+    rk_model_gen = True
+    config={
+        "thresh": 0.5,
+        "max_candidates": 999999,
+        "min_size": 0.002,
+        "box_thresh": 0
+    }
+    deteor = SegDetector(**config)
     # Create RKNN object
     rknn = RKNN(verbose=True)
 
@@ -57,32 +183,37 @@ if __name__ == '__main__':
     # [[1,3,1728,1280]],    # 第二个输入形状
     [[1,3,1728,1728]] 
     ]
-    rknn.config(mean_values=[103.94, 116.78, 123.68], 
-            std_values=[58.82, 58.82, 58.82], 
+    rknn.config(#mean_values=[103.94, 116.78, 123.68], 
+            #std_values=[58.82, 58.82, 58.82], 
             quant_img_RGB2BGR=True, 
             dynamic_input=dynamic_input, target_platform='rk3588')
     ##################################################################
-    ret = rknn.load_onnx(model=ONNX_MODEL,)
-    if ret != 0:
-        print('Load model failed!')
-        exit(ret)
-    print('done')
+    if rk_model_gen:
+        ret = rknn.load_onnx(model=ONNX_MODEL,)
+        if ret != 0:
+            print('Load model failed!')
+            exit(ret)
+        print('done')
 
     # Build model
     print('--> Building model')
-    ret = rknn.build(do_quantization=QUANTIZE_ON, dataset=DATASET)
-    if ret != 0:
-        print('Build model failed!')
-        exit(ret)
-    print('done')
+    if rk_model_gen:
+        ret = rknn.build(do_quantization=QUANTIZE_ON, dataset=DATASET)
+        if ret != 0:
+            print('Build model failed!')
+            exit(ret)
+        print('done')
 
-    # Export RKNN model
-    print('--> Export rknn model')
-    ret = rknn.export_rknn(RKNN_MODEL)
-    if ret != 0:
-        print('Export rknn model failed!')
-        exit(ret)
-    print('done')
+        # Export RKNN model
+        print('--> Export rknn model')
+        ret = rknn.export_rknn(RKNN_MODEL)
+        if ret != 0:
+            print('Export rknn model failed!')
+            exit(ret)
+        print('done')
+    else:
+        ret = rknn.build(do_quantization=QUANTIZE_ON, dataset=DATASET)
+        rknn.load_rknn("/usr/projects/explore/rknn-toolkit2/rknn-toolkit2/examples/onnx/Db-mul/final_2024_05_05_17_02.rknn")
     #################################################  编写具体的图像处理流程 ############################################
     # Init runtime environment
     ret = rknn.init_runtime()
@@ -102,71 +233,23 @@ if __name__ == '__main__':
         new_width = image_leng_side
         new_height = int(math.ceil(new_width / img_h * img_w / 32) * 32)
         
-    img_infoes = letterbox(img,new_shape=(1728,1728))
+    img_infoes = letterbox(img,new_shape=(1728,1728),color=(125,125,125))
     im, ratio, (dw, dh) = img_infoes
+    im = im - np.array([122.67891434, 116.66876762, 104.00698793])
     img2 = np.expand_dims(im, 0)
+    img2 = img2/255.
+    img2 = img2.astype(np.float32)
     rknn_infere_result = rknn.inference(inputs=[img2], data_format=['nhwc'])
-    print(f'outputs:{rknn_infere_result}')
+    # print(f'outputs:{rknn_infere_result}')
     pred, mcls = rknn_infere_result[0], rknn_infere_result[1]
-    # ---------------------- ----------------------------------------
-    # def softmax(x, axis):
-    #     x -= np.max(x, axis=axis, keepdims=True)
-    #     f_x = np.exp(x) / np.sum(np.exp(x), axis=axis, keepdims=True)
-    #     return f_x
-    # b, _, w, h = mcls.shape
-    # out_class = mcls.transpose(0, 2, 3, 1).reshape(-1, self.class_num)
-    # out_class = softmax(out_class, -1)
-    # out_class = np.expand_dims(out_class.max(1).reshape(b, w, h),axis=1)
-    # boxes, scores, classes = self.structure.representer.represent(batch, pred, out_class,
-    #                                                                 is_output_polygon=poly_flag)
+    print(f'pred:{pred}')
+    detor_info = deteor.represent(pred, (img_h, img_w))
+    detect_boxes = detor_info[0]
     
-    
-    
-    
-    ####################################################################################################################
-    # # Init runtime environment
-    # print('--> Init runtime environment')
-    # ret = rknn.init_runtime()
-    # if ret != 0:
-    #     print('Init runtime environment failed!')
-    #     exit(ret)
-    # print('done')
-
-    # # Set inputs
-    # img = cv2.imread(IMG_PATH)
-    # # img, ratio, (dw, dh) = letterbox(img, new_shape=(IMG_SIZE, IMG_SIZE))
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-
-    # # Inference
-    # print('--> Running model')
-    # img2 = np.expand_dims(img, 0)
-    # outputs = rknn.inference(inputs=[img2], data_format=['nhwc'])
-    # np.save('./onnx_yolov5_0.npy', outputs[0])
-    # np.save('./onnx_yolov5_1.npy', outputs[1])
-    # np.save('./onnx_yolov5_2.npy', outputs[2])
-    # print('done')
-
-    # # post process
-    # input0_data = outputs[0]
-    # input1_data = outputs[1]
-    # input2_data = outputs[2]
-
-    # input0_data = input0_data.reshape([3, -1]+list(input0_data.shape[-2:]))
-    # input1_data = input1_data.reshape([3, -1]+list(input1_data.shape[-2:]))
-    # input2_data = input2_data.reshape([3, -1]+list(input2_data.shape[-2:]))
-
-    # input_data = list()
-    # input_data.append(np.transpose(input0_data, (2, 3, 0, 1)))
-    # input_data.append(np.transpose(input1_data, (2, 3, 0, 1)))
-    # input_data.append(np.transpose(input2_data, (2, 3, 0, 1)))
-
-    # boxes, classes, scores = yolov5_post_process(input_data)
-
-    # img_1 = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    # if boxes is not None:
-    #     draw(img_1, boxes, scores, classes)
-    #     cv2.imwrite('result.jpg', img_1)
-    #     print('Save results to result.jpg!')
-
+    for box in detect_boxes:
+        bbox = np.array(box,np.int32)
+        print(f'bbox:{bbox}')
+        cv2.polylines(img, [bbox], True, (0, 255, 0), 3) 
+    cv2.imwrite('res.jpg', img)
+    # print(f'detor_info:{detor_info}')
     rknn.release()
