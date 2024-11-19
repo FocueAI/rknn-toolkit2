f8a3892d81a1295e39560c3b00697a37cc606af9
FocueAI <17051325951@163.com>
2024-11-19 09:54:10 +0800

    dbnet可以在3588上跑通

--- rknn-toolkit2/examples/onnx/Db-mul/test.py
+++ rknn-toolkit2/examples/onnx/Db-mul/test.py
@@ -21,6 +21,7 @@ QUANTIZE_ON =  False
 def letterbox(im, new_shape=(640, 640), color=(0, 0, 0)):
     # Resize and pad image while meeting stride-multiple constraints
     shape = im.shape[:2]  # current shape [height, width]
+    old_h, old_w = shape
     if isinstance(new_shape, int):
         new_shape = (new_shape, new_shape)
 
@@ -37,10 +38,24 @@ def letterbox(im, new_shape=(640, 640), color=(0, 0, 0)):
 
     if shape[::-1] != new_unpad:  # resize
         im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+        new_h, new_w = im.shape[:2]
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
     im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return im, ratio, (dw, dh)
+    img_info = {
+        "im": im,
+        'w_ratio': old_w/new_w,
+        'h_ratio': old_h/new_h,
+        'old_w': old_w,
+        'old_h': old_h,
+        'new_w': new_w,
+        'new_h': new_h,
+        'top': top,
+        'bottom': bottom,
+        'left': left,
+        'right': right
+    }
+    return img_info
 
 
 class SegDetector:
@@ -84,7 +99,7 @@ class SegDetector:
         expanded = np.array(offset.Execute(distance))
         return expanded
     
-    def box_from_bitmap(self, pred, origin_im_size):
+    def box_from_bitmap(self, pred, img_infoes):
         bitmap = self.binarize(pred)[0][0]
         pred = pred[0]
         height, width = pred.shape[1:]
@@ -111,8 +126,8 @@ class SegDetector:
             score = self.box_score_fast(pred[0], points.reshape(-1, 2))     
             if score < self.box_thresh:
                 continue
-            box = self.unclip(points, unclip_ratio=0.8).reshape(-1, 1, 2)
-            ############ hou 2024-4-22 #############
+            box = self.unclip(points, unclip_ratio=1.2).reshape(-1, 1, 2)
+            ##########################################
             box_ = np.squeeze(box)
             if Polygon(box_).area < 12000:
                    continue
@@ -120,11 +135,9 @@ class SegDetector:
             box = np.array(box)
             if sside < self.min_size + 2:
                 continue
-            #########################################
-            box[:, 0] = np.clip(
-                np.round(box[:, 0] / width * origin_im_size[1]), 0, origin_im_size[1])
-            box[:, 1] = np.clip(
-                np.round(box[:, 1] / height * origin_im_size[0]), 0, origin_im_size[0])
+            ######################################### img_infoes
+            box[:, 0] = np.clip( (np.round(box[:, 0] - img_infoes["left"] ) / img_infoes["new_w"] * img_infoes["old_w"] ) , 0, img_infoes["old_w"])
+            box[:, 1] = np.clip( (np.round(box[:, 1] - img_infoes["top"]  ) / img_infoes["new_h"] * img_infoes["old_h"]), 0, img_infoes["old_h"])
             ####################################
             box, sside = self.get_mini_boxes(box)
             box = np.array(box)
@@ -150,8 +163,8 @@ class SegDetector:
 
         return cv2.mean(bitmap[ymin:ymax + 1, xmin:xmax + 1], mask)[0]
     
-    def represent(self, pred, origin_im_size):
-       boxes, scores = self.box_from_bitmap(pred, origin_im_size)
+    def represent(self, pred, img_infoes):
+       boxes, scores = self.box_from_bitmap(pred, img_infoes)
        return boxes, scores
 
 
@@ -172,16 +185,17 @@ if __name__ == '__main__':
 
     # pre-process config
     print('--> Config model')
-    # rknn.config(mean_values=[[0, 0, 0]], std_values=[[255, 255, 255]], target_platform='rk3588')
     print('done')
 
     # Load ONNX model
     print('--> Loading model')
     ########################## 增加一些其他配置 ########################
+    # image_leng_side = 1728  # 由于该平台的局限性，先这样设置
+    image_leng_side = 1280
     dynamic_input = [
     # [[1,3,1280,1728]],    # 第一个输入形状
     # [[1,3,1728,1280]],    # 第二个输入形状
-    [[1,3,1728,1728]] 
+    [[1,3,image_leng_side,image_leng_side]] 
     ]
     rknn.config(#mean_values=[103.94, 116.78, 123.68], 
             #std_values=[58.82, 58.82, 58.82], 
@@ -225,7 +239,7 @@ if __name__ == '__main__':
     img = cv2.imread(IMG_PATH)
     
     img_h, img_w = img.shape[:2]
-    image_leng_side = 1728  # 由于该平台的局限性，先这样设置
+    
     if img_h > img_w: # 1280,1728
         new_height = image_leng_side
         new_width = int(math.ceil(new_height / img_h * img_w / 32) * 32)
@@ -233,9 +247,9 @@ if __name__ == '__main__':
         new_width = image_leng_side
         new_height = int(math.ceil(new_width / img_h * img_w / 32) * 32)
         
-    img_infoes = letterbox(img,new_shape=(1728,1728),color=(125,125,125))
-    im, ratio, (dw, dh) = img_infoes
-    im = im - np.array([122.67891434, 116.66876762, 104.00698793])
+    img_infoes = letterbox(img,new_shape=(image_leng_side,image_leng_side),color=(125,125,125))
+    # im, ratio, (top, _, left,  _) = img_infoes
+    im = img_infoes["im"] - np.array([122.67891434, 116.66876762, 104.00698793])
     img2 = np.expand_dims(im, 0)
     img2 = img2/255.
     img2 = img2.astype(np.float32)
@@ -243,7 +257,7 @@ if __name__ == '__main__':
     # print(f'outputs:{rknn_infere_result}')
     pred, mcls = rknn_infere_result[0], rknn_infere_result[1]
     print(f'pred:{pred}')
-    detor_info = deteor.represent(pred, (img_h, img_w))
+    detor_info = deteor.represent(pred, img_infoes)
     detect_boxes = detor_info[0]
     
     for box in detect_boxes:
